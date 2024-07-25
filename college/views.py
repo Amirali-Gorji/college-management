24422bb559549a6af0f0d2a6c81b160a0e658749
Amirali Gorji <amirali.gorji@gmail.com>
2024-07-25 18:53:30 +0330

    :sparkles: feat: Add required permissions to views

--- college/views.py
+++ college/views.py
@@ -14,37 +14,59 @@ from college.serializers import (
     OfferCourseSerializer,
     StudentClassSerializer,
 )
+from users.permissions import CollegeAuthentication
 
-
-class UserCreateView(APIView):
-    # TODO: endpoint for superuser to create teacher and student
-    def post(self, request, *args, **kwargs):
-        pass
 
 
 class SemesterView(ModelViewSet):
+    required_permissions = {
+        'list': 'can_view_semester',
+        'retrieve': 'can_view_semester',
+        'update': 'can_update_semester',
+        'destroy': 'can_delete_semester',
+        'create': 'can_add_semester',
+    }
+    
+    permission_classes = [CollegeAuthentication]
     queryset = Semester.objects.all()
     serializer_class = SemesterSerializer
 
 
 class CourseView(ModelViewSet):
+    required_permissions = {
+        'list': 'can_view_course',
+        'retrieve': 'can_view_course',
+        'update': 'can_update_course',
+        'destroy': 'can_delete_course',
+        'create': 'can_add_course',
+    }
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
 class OfferCourseView(ModelViewSet):
+    required_permissions = {
+        'list': 'can_view_offer_course',
+        'retrieve': 'can_view_offer_course',
+        'update': 'can_update_offer_course',
+        'destroy': 'can_delete_offer_course',
+        'create': 'can_add_offer_course',
+    }
     queryset = OfferCourse.objects.all()
     serializer_class = OfferCourseSerializer
 
 
 class StudentClassView(ModelViewSet):
+    required_permissions = {
+        'list': 'can_view_student_class',
+        'retrieve': 'can_view_student_class',
+        'update': 'can_update_student_class',
+        'destroy': 'can_delete_student_class',
+        'create': 'can_add_student_class',
+    }
     queryset = StudentClass.objects.all()
     serializer_class = StudentClassSerializer
 
-
-class StudentView(ModelViewSet):
-    pass
-    # queryset = User.obj
 
 
 
