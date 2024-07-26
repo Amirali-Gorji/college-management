a9a65bf81c0fd467c5ee0a74bf548009a27ffea9
Amirali Gorji <amirali.gorji@gmail.com>
2024-07-26 19:34:31 +0330

    :sparkles: feat: Add expire time for jwt token to settings

--- college_management/settings.py
+++ college_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,12 +105,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'college.CollegeUser'
+AUTH_USER_MODEL = 'users.CollegeUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
 
 # Internationalization
