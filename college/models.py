654161b2d0bfa5748c08bd89a270e53765510935
Amirali Gorji <amirali.gorji@gmail.com>
2024-07-26 19:33:29 +0330

    :sparkles: feat: Add some columns to college models

--- college/models.py
+++ college/models.py
@@ -10,12 +10,18 @@ class Semester(models.Model):
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
+    unit = models.IntegerField()
 
 
 class OfferCourse(models.Model):
+    class_status_choices = (
+        ('active', 'Active'),
+        ('deactive', 'Deactive'),
+    )
     semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.PROTECT)
     teacher = models.ForeignKey(CollegeUser, on_delete=models.PROTECT)
+    class_status = models.CharField(choices=class_status_choices, max_length=100)
     desc = models.TextField()
 
     class Meta:
@@ -25,9 +31,12 @@ class OfferCourse(models.Model):
 class StudentClass(models.Model):
     offer_course = models.ForeignKey(OfferCourse, on_delete=models.CASCADE)
     student = models.ForeignKey(CollegeUser, on_delete=models.PROTECT)
+    rate = models.CharField(max_length=10)
 
     class Meta:
         unique_together = ('offer_course', 'student')
+
+
 
 
 
